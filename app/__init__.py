88bd391c7d9961e16218d283418e207cab8ebe50
Odili Chinedum Christian <odili.official@gmail.com>
2024-02-11 10:20:18 +0100

    updated the config file

--- app/__init__.py
+++ app/__init__.py
@@ -1,46 +1,67 @@
 # Global Imports
 import os
 from flask import Flask
-# from config import Config
 
 # This will create and configure the app
 def create_app(test_config=None):
     # creating the app instance
     app = Flask(__name__, instance_relative_config=True)
-    
-    app.config.from_mapping(
-        SECRET_KEY = 'this is not a good secret key don\'t forget to change it',
-        DATABASE = os.path.join(app.instance_path, 'todo.sqlite'),
-    )
-    
+
     if test_config is None:
-        # This will load the instance configuration from the config.py file 
-        # if it exist or create it if it doesn't exist when not testing
-        
-        file_path = os.path.join('./app', 'config.py')
+         # This will ensure the instance folder exists. if it doesn't exist it will be created.
+        try:
+            os.makedirs(app.instance_path)
+        except OSError:
+            pass
+
+        # This will load the instance configuration from the config.py file
+        # if it exist or create it if it doesn't exist when not testing.
+
+        file_path = os.path.join('./instance', 'config.py')
+
+        content_file_path = os.path.join('./','.copy_config.txt')
+
         if not os.path.exists(file_path):
-            # create an empty file
-            with open(file_path, 'w'):
-                pass
-        
-        app.config.from_pyfile('config.py', silent=True)
+            # create the file if it doesn't exist
+            with open(file_path, 'w') as new_file:
+
+                # Read the content of the .copy_config.txt file
+                with open(content_file_path, 'r') as content_file:
+                    content = content_file.read()
+
+                    # write the content of the .copy_config.txt file to the config.py file
+                    new_file.write(content)
+
+        else:
+            # if the files exists, it will read the content of the config.py file and compare it with the content of the .copy_config.txt file.
+
+            with open(file_path, 'r') as new_file:
+                config_file = new_file.read()
+            with open(content_file_path, 'r') as content_file:
+                content = content_file.read()
+                if config_file != content:
+                    # if the content of the config.py file doesn't match the content of the .copy_config.txt file, it will update the config.py file with the content of the .copy_config.txt file. Plese note that the server need to restart after updating the .copy_config.txt file
+                    with open(file_path, 'w') as new_file:
+                        new_file.write(content)
+
+        # importing the config class
+        from instance.config import Config
+        # This method is used to load configuration values from a Python object and initializing apllication configuration.
+        app.config.from_object(Config)
+        # app.config.from_pyfile('config.py', silent=True)
+
+
     else:
-        # if the config.py file doesn't exist or if the app is in testing it will load the default text_configuration
-        
+        # if the app is in testing, it will load the default test configuration
+
         app.config.from_mapping(test_config)
-        
-    # This will ensure the instance folder exists. if it doesn't exist it will be created.
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
-    
-    # this will show a simple page that says hello 
+
+    # this will show the root directory of the app
     @app.route('/')
-    @app.route('/hello')
+    # @app.route('/hello')
     def hello():
         current_directory = os.getcwd()
         # show = print(current_directory)
         return current_directory
-    
+
     return app
